fa82e4c2df6e3b58f9f7d1ad4e2497ef66ac8fb8
hyoogu <idhyo0o@gmail.com>
2021-07-16 18:39:44 +0900

    Add postUsers

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,18 @@
-import { Controller, Get, Post, Req, Res } from '@nestjs/common';
-import { response } from 'express';
+import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { JoinRequestDto } from './dto/join.request.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+	constructor(private usersService: UsersService) { }
+
 	@Get()
 	getUsers(@Req() req) {
 		return req.user;
 	}
 	@Post()
-	postUsers() {
+	postUsers(@Body() data: JoinRequestDto) {
+		this.usersService.postUsers(data.email, data.nickname, data.password);
 	}
 
 	@Post('login') // POST users/login
@@ -19,7 +23,7 @@ export class UsersController {
 	@Post('logout')
 	logOut(@Req() req, @Res() res) {
 		req.logOut();
-		response.clearCookie('connect.sid', { htppOnly: true });
+		res.clearCookie('connect.sid', { htppOnly: true });
 		res.send('ok');
 	}
 }
